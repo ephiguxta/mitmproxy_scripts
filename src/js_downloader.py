7129867c166107eee5cb97c6ad4296dc1095dc65
ephiguxta <fabiogustavo@tuta.io>
2025-11-22 17:17:04 -0400

    formatando o código com black

--- src/js_downloader.py
+++ src/js_downloader.py
@@ -4,40 +4,40 @@ from brotli import decompress as br_decompress
 
 # Obs: é preciso utilizar o "Match and Replace" do Burp e trocar para "Accept-Encoding: gzip"
 
+
 def response(flow):
-	headers = flow.response.headers
-	path = flow.request.path
+    headers = flow.response.headers
+    path = flow.request.path
 
-	header_sign = "Content-Type"
+    header_sign = "Content-Type"
 
-	if header_sign in headers:
-		match = search(r'(text|application)/(x-|)javascript', headers[header_sign])
+    if header_sign in headers:
+        match = search(r"(text|application)/(x-|)javascript", headers[header_sign])
 
-		if match is not None:
-				# pega apenas o nome do arquivo JavaScript
-				match = search(r'(?<=/)(\w|[_-])+.js($|(?=\?))', path)
+        if match is not None:
+            # pega apenas o nome do arquivo JavaScript
+            match = search(r"(?<=/)(\w|[_-])+.js($|(?=\?))", path)
 
-				if match is not None:
+            if match is not None:
 
-					with open(match.group(0), "w") as f:
+                with open(match.group(0), "w") as f:
 
+                    # caso for um JS puro
+                    if not "Content-Encoding" in headers:
+                        print(flow.response.data.content.decode("utf-8"), file=f)
+                        return
 
-						# caso for um JS puro
-						if not "Content-Encoding" in headers:
-							print(flow.response.data.content.decode("utf-8"), file=f)
-							return
+                    content_encoding = headers["Content-Encoding"]
 
-						content_encoding = headers["Content-Encoding"]
+                    # caso haja compressão no arquivo, seja ele gzip ou br...
+                    # descomprime e joga pro arquivo local
 
-						# caso haja compressão no arquivo, seja ele gzip ou br...
-						# descomprime e joga pro arquivo local
+                    if content_encoding == "gzip":
+                        # arquivo comprimido em GZIP
+                        decompressed_js = gzip_decompress(flow.response.data.content)
 
-						if content_encoding == "gzip":
-							# arquivo comprimido em GZIP
-							decompressed_js = gzip_decompress(flow.response.data.content)
+                    elif content_encoding == "br":
+                        # arquivo comprimido em Brotli
+                        decompressed_js = br_decompress(flow.response.data.content)
 
-						elif content_encoding == "br":
-							# arquivo comprimido em Brotli
-							decompressed_js = br_decompress(flow.response.data.content)
-
-						print(decompressed_js.decode("utf-8"), file=f)
+                    print(decompressed_js.decode("utf-8"), file=f)
